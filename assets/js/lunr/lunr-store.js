5952c45fec9658826822e7eed9ab0d4b805b750b
doughertym <doughertym@users.noreply.github.com>
2024-03-28 01:18:12 +0000

    jekyll build from Action f3fb1526c4cd465e465d1f803bb710d34f9163c6

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Hello World",
+        "excerpt":"Hello World!   Here is a video that I enjoy.     And now, I have edited the file on my iPad (using Working Copy) to see how to get it published.  ","categories": [],
+        "tags": [],
+        "url": "/2021/01/25/hello-world.html",
+        "teaser": null
+      },{
+        "title": "Protect yourself from leftism!",
+        "excerpt":"You made some notes in your personal journal on 03 and 04/Jan/2024 about how Corgibytes has been these last few months. It seems their leftist ideology has corrupted their minds. It seems that they are either in a state of disbelief or something as they do not seem to be...","categories": ["employment"],
+        "tags": ["work","politics","freedom"],
+        "url": "/employment/2024/01/01/Protect-yourself-from-leftism.html",
+        "teaser": null
+      }]
