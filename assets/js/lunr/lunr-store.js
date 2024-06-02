861dddd59694e4504bb5abb83e2d879376358385
doughertym <doughertym@users.noreply.github.com>
2024-06-02 01:36:14 +0000

    jekyll build from Action 9079642f971d4a2ccf5b0315bfef9826d12ad8ef

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Hello World",
+        "excerpt":"Hello World!   Here is a video that I enjoy.     And now, I have edited the file on my iPad (using Working Copy) to see how to get it published.  ","categories": [],
+        "tags": [],
+        "url": "/2021/01/25/hello-world.html",
+        "teaser": null
+      }]
